957c97c33d94f00e0219ac51086b87cc2469228a
aganyo360 <wilsonaganyo@gmail.com>
2024-03-24 12:25:00 +0300

    back and forward navs

--- navigation/nav.py
+++ navigation/nav.py
@@ -3,13 +3,26 @@ from selenium.webdriver.edge.service import Service
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 import time  
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
 
 PATH = r'C:\Program Files (x86)\msedgedriver.exe'
-
 edge_options = webdriver.EdgeOptions()
-
 edge_service = Service(PATH)
 driver = webdriver.Edge(service=edge_service, options=edge_options)
+driver.get("https://codewithmosh.com/")
+link = driver.find_element(By.LINK_TEXT, 'Courses')
+link.click()
 
-driver.get("https://www.jumia.co.ke")
-link = driver.find_element_by_link_text("Fashion")
+
+try:
+    element = WebDriverWait(driver, 15).until(
+        EC.presence_of_element_located((By.LINK_TEXT, 'Courses'))
+    )
+    element.clear()
+    element.click()
+    driver.back()
+    driver.forward()
+except:
+    driver.quit()
